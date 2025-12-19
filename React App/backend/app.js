266c9b339816822269c74859dfa8ca19c4d4723a
Zidane86-06 <kungloa75@gmail.com>
2025-12-19 09:53:24 +0530

    done

--- React App/backend/app.js
+++ React App/backend/app.js
@@ -10,7 +10,11 @@ const authRoutes = require("./routes/authRoutes");
 const app = express();
 
 // middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin:"http://18.234.144.171:7000",credentials:true,
+    }
+));
 app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
